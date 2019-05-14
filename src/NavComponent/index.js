574638782ec5e6a540d6dcd83a0e8b4e2585dc66
jschaef80 <jschaef80@gmail.com>
2019-05-13 20:36:34 -0500

    Final commit

--- src/NavComponent/index.js
+++ src/NavComponent/index.js
@@ -5,7 +5,9 @@ export default class LeftComponent extends React.Component{
     render(){
         return(
             <div className="nav">
-
+                <div className="left">Home Moments Notifications Messages</div>
+                <div className="middle"></div>
+                <div className="right"><input type="text" placeholder="Search Twitter"></input><button>Tweet</button></div>
             </div>
         )
     }
